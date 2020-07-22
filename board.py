c6f1c4e6ce30cb53e6ba6d12fe74cfe26a16748d
HanoteauN <hanoteaunathan@gmail.com>
2020-07-22 09:15:05 +0200

    All black pieces

--- board.py
+++ board.py
@@ -3,6 +3,7 @@ from pawn import pawn
 class board:
     def __init__(self, dimension ):
         self.dimension= dimension
+        #board is an empty dictionary
         board1 = {}
         
         self.board1= board1
@@ -11,8 +12,10 @@ class board:
         else:
              nbrPawns=37
         self.nbrPawns= nbrPawns
+        #to have lowercase letters on the y axis.
         yAxis= string.ascii_lowercase[:dimension]
         self.yAxis= yAxis
+        
         for i in yAxis:
             for j in range(1, dimension+1):
                 board1[i+str(j)]='empty' 
@@ -21,18 +24,22 @@ class board:
         blackPawns=  (self.nbrPawns-1)*2//3 + 1
         #only for the black pawns on top and bottom without the middle ones
         count=0
-        for i in range(4, size-3):
+        #size-3 +1 cause size-3 is not included.
+        for i in range(4, size-3 +1):
             self.board1['a'+str(i)]= 'P'+ str(i-3)
             self.board1[self.yAxis[-1]+str(i)]='P'+ str( blackPawns - (i-3)) 
             count+=1 
-#        for i in range(self.yAxis[3], self.yAxis[size-4] ):
-        for i in range( 3, size-4 ):
+# previous  for i in range(self.yAxis[3], self.yAxis[size-4] ):
+        # size-4 cause yAxis[] start at index 0 but our letters start at 1
+        for i in range( 3, size-4 + 1 ):
             cpt=1
             self.board1[self.yAxis[i]+ '1' ]= 'P'+str( cpt +count)
             self.board1[self.yAxis[i]+ str(size)]= 'P'+ str(blackPawns-count -cpt )
             cpt+=1
-
+        #b5 for S9
         self.board1[ 'b'+ str( size//2 +1)] = 'P'+str(blackPawns//2-1)
+        #h5 for size 9
         self.board1[ self.yAxis[-2] + str(size//2+1) ] = 'P'+str(blackPawns//2)
-        self.board1[ self.yAxis[size//2] + '1' ] = 'P'+str(blackPawns//2+1)
+        #e2 for S9
+        self.board1[ self.yAxis[size//2] + '2' ] = 'P'+str(blackPawns//2+1)
         self.board1[ self.yAxis[size//2] + str(size-1) ] = 'P'+str(blackPawns//2+2)
