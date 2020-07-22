92855b3064dd93571f93800a47e79b872597d1a1
HanoteauN <hanoteaunathan@gmail.com>
2020-07-23 00:45:42 +0200

    Moves all directions

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def main():
     p= player('white', b)
     p2= player('black', b)
     b.setPieces(b.dimension)
-
+    print(b.board1)
+    b.moves('d1', 'b1')
+    b.moves('d9', 'd7')
     
     print(b.board1)
     print(b.nbrPawns)
